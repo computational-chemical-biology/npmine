be1d363ab9f7a9a0775a33b1b3a70d3ada6312e1
rsilva <rsilva@seriema>
2020-09-16 10:52:31 -0300

    changing bioactivity

--- npmine/get_bioactivity.py
+++ npmine/get_bioactivity.py
@@ -13,11 +13,32 @@ def get_bioactivity(queries, chunksize=50):
     ----------
     """
     jlist = []
-    for i in range(0, len(queries), chunksize):
+    for i in range(46100, len(queries), chunksize):
         to_query = queries[i:i+chunksize]
         ids = ','.join(map(str, to_query.tolist()))
         url = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/%s/assaysummary/JSON' % ids
         rq = requests.get(url)
-        jlist.append(rq.json())
+        if rq.status_code==200:
+            jlist.append(rq.json())
+        else:
+            rq.raise_for_status()
 
     return jlist
+
+def dict2pd(biotable):
+    """Converts bioactivity dict to dataframe
+    Parameters
+    ----------
+    biotable: dict
+        PubChem's activity dictionary.
+    Returns
+        Pandas DataFrame
+    ----------
+    """
+    rows = []
+    for row in biotable['Table']['Row']:
+        rows.append(row['Cell'])
+
+    df = pd.DataFrame(rows)
+    df.columns = biotable['Table']['Columns']['Column']
+    return df
