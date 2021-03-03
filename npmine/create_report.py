dd8cb4ff16954e0874fb19761b12ad6dc16db69c
rsilva <ridasilva@usp.br>
2021-03-03 10:17:57 -0300

    adding missing import

--- npmine/create_report.py
+++ npmine/create_report.py
@@ -9,6 +9,7 @@ import pandas as pd
 import numpy as np
 from IPython.display import SVG
 import matplotlib.pyplot as plt
+from npmine.retrieve_ID_pubchem_spiderchem import inchikey2cid
 
 html = '''<!DOCTYPE html>
 <html>
